f3d66292d3559673cf4a8eeed33776af09c6702d
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2022-03-04 03:31:57 -0500

    Add weather behavior

---
+++ Jarvis/Behaviors/WeatherBehavior.cs
@@ -0,0 +1,24 @@
+﻿using Jarvis.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis.Behaviors
+{
+    public class WeatherBehavior : IWebUpdate
+    {
+        public bool Enabled { get; set; } = false;
+        public int Priority => 150;
+
+        public void WebUpdate()
+        {
+            JarvisRequest[] requests = ComSystem.Requests();
+            if (requests.Length > 0)
+            {
+                
+            }
+        }
+    }
+}
