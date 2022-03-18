09e6acb244b72b531467b8c3bebb78c7c78b435a
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2022-03-18 14:59:12 -0400

    Testing updates

---
+++ Jarvis/Behaviors/TestBehavior.cs
@@ -0,0 +1,21 @@
+﻿using Jarvis.API;
+
+namespace Jarvis.Behaviors
+{
+    public class TestBehavior : IStart, IWebUpdate
+    {
+        public bool Enabled { get; set; } = true;
+        public int Priority => 200;
+
+        public void Start()
+        {
+            FileSystem.WriteFileText(this, "TestJarvisFile.txt", "Test text");
+        }
+
+        public void WebUpdate()
+        {
+            string fileTxt = FileSystem.GetFileText(this, "TextJarvisFile.txt");
+            Log.Info(fileTxt);
+        }
+    }
+}
